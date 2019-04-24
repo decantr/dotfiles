38584cbcabcb77f862130710f7d35665591d1c79
decantr <theeasterlywood@gmail.com>
2019-04-24 20:50:28 +0100

    reset gaps to right size

--- change.sh
+++ change.sh
@@ -28,7 +28,7 @@ sed -i 's/gaps inner 15/gaps inner 5/' .config/i3/config
 sed -i 's/gaps outer 15/gaps outer 0/' .config/i3/config
 sed -i 's/exec --no-startup-id xcompmgr//' .config/i3/config
 sed -i 's/bindsym $mod+w			exec $term -e nmtui/bindsym $mod+w			exec networkmanager_dmenu/' .config/i3/config
-
+sed -i 's/gaps inner current set 15; gaps outer current set 15/gaps inner current set 5; gaps outer current set 0/' .config/i3/config
 # i3blocks
 # replace "[help] \n interval=once" with ""
 
