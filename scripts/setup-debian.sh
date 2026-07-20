d51d7a25cdb9a76408eea0024a538d8f513153f2
Shaw <eastwoodshaw@gmail.com>
2026-07-21 00:13:40 +0100

    feat: add debian setup script

---
+++ scripts/setup-debian.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+
+# Install Proxy
+printf 'Acquire::http::Proxy "http://10.0.0.112:3142";
+Acquire::https::Proxy "DIRECT";' | sudo tee /etc/apt/apt.conf.d/20proxy
+
+# Install Apps
+sudo apt update
+sudo apt install -y \
+	openssh-server \
+	curl \
+	\
+	tmux \
+	vim \
+	fastfetch \
+	btop \
+	nnn \
+	\
+	sway \
+	swayidle \
+	swaylock \
+	brightnessctl \
+	foot \
+	grim \
+	slurp \
+	wireplumber \
+	xdg-desktop-portal-wlr \
+	xwayland
