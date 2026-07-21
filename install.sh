510e866c53ec3563fcd7352cf1614fa4db5bdace
Shaw <eastwoodshaw@gmail.com>
2026-07-21 17:06:13 +0100

    feat: add basic install script

---
+++ install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+#
+# curl -fsSl "https://raw.githubusercontent.com/decantr/dotfiles/refs/heads/master/install.sh" | sh
+
+set -e
+
+# Check for Dotfiles repo
+if [ ! -d ~/.local/share/dotfiles ]; then
+	echo "!! No dotfiles"
+	exit 1
+fi
+
+# Install Mise if not available
+# @todo
+
+# Remove existing Mise config
+if [ -f ~/.config/mise/config.toml ]; then
+	echo ":: Backing up existing \`mise.toml\`"
+
+	mv ~/.config/mise/config.toml ~/.config/mise/config.toml."$(date +%Y%m%d-%H%M%S)"
+fi
+
+#
+echo ":: Applying "
+
+mise dotfiles apply ~/.config/mise/config.toml
